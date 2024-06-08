104460f8c8a8fb201ea9f8cbe6cfa5c7b4bf1c33
Nnorto <artem4icaz@gmail.com>
2024-06-08 08:55:07 +0300

    Решение

--- 27 задание/ЕГЭ 2021 МАКСИМАЛЬНАЯ ПРЕФ СУММА И МАКС ДЛИНА.py
+++ 27 задание/ЕГЭ 2021 МАКСИМАЛЬНАЯ ПРЕФ СУММА И МАКС ДЛИНА.py
@@ -1,5 +1,31 @@
 f = open('27 задание/27 файлы/27_A_префикс 263.txt')
 n = int(f.readline())
+a = [int(x) for x in f]
+k = 100
+s = 0
+maxs, maxd = 0, 0
+prefsnsm = 10**100
+preflen = 0
+
+for i in range(len(a)):
+    s += a[i]
+    if s % 2 == 0:
+        dl = i + 1
+        if maxs < s or (maxs == s and maxd < dl):
+            maxs = s
+            maxd = dl
+    else:
+        vs = s - prefsnsm
+        maxd = max(maxd, i - prefsnsm + 1)
+
+    if prefsnsm > s:
+        prefsnsm = s
+        preflen = i + 1
+print(maxs, maxd)
+
+# ИЛИ
+f = open('27 задание/27 файлы/27_A_префикс 263.txt')
+n = int(f.readline())
 k = 2
 a = [int(x) for x in f]
 pref = [10 ** 100] * k
