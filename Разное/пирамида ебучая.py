28479ae6a890804378cadf302397898dbb0bb153
Nnorto <artem4icaz@gmail.com>
2024-09-26 21:33:54 +0300

    Решение

--- Разное/пирамида ебучая.py
+++ Разное/пирамида ебучая.py
@@ -1,15 +1,19 @@
-b = input()
+N = int(input('КОЛИЧЕСТВО СТУПЕНЕЙ: '))
+b = input('Ваша буква: ')
 t = ' '
-c = 7
+n = N * 2
+c = (n//2) + 1
 num = ord(b)
-for i in range(1, 10+1, 2):
+for i in range(1, n+1, 2):
     s = ''
     mid = (i//2)+1
-    for j in range(mid):
-        s += chr(num+j)
+    s += ''.join([chr(num+j) for j in range(mid)])
+    # for j in range(mid):
+    #     s += chr(num+j)
     c += -1
     r = ''
     if i > 1:
-        for x in range(mid+1, i+1):
-            s += chr(num+(i-x))
+        s += ''.join([chr(num+(i-x)) for x in range(mid+1, i+1)])
+        # for x in range(mid+1, i+1):
+        #     s += chr(num+(i-x))
     print(c * t + s)
